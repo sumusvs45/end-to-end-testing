111d7f67094980eb20750aef71e547a1c5a658fd
sumanthsigirisetty <sumanth.sigirisetty@sanjittechsolutions.in>
2025-01-22 11:15:31 +0530

    Update auth.js

--- Api/auth/auth.js
+++ Api/auth/auth.js
@@ -9,13 +9,13 @@ const auth = async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, 'secret@key');
-    const user = await userModel.findById(decoded.userId);
+    const user = await userModel.findById(decoded.userId);//log decoded to check payload
     
     if (!user) {
       return res.status(404).json({ success: false, message: "User not found" });
     }
     
-    req.user = user.id;
+    req.user = user.id;//log getting user id from database
     next();
   } catch (error) {
     return res.status(400).json({ success: false, message: "Invalid or expired token" });
